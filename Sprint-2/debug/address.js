05984c9fd07ff5208856bacdf0f07f14253d5ca8
Anastasiia Hlushkova <glushkova.a.y@gmail.com>
2026-01-15 20:59:45 +0100

    Sprint 2: debug exercises

--- Sprint-2/debug/address.js
+++ Sprint-2/debug/address.js
@@ -1,4 +1,6 @@
-// Predict and explain first...
+// Predict and explain first
+// My house number will be undefined. address is an object, not an array.
+// Objects do not have indices like arrays do.The expression: address[0] attempts to retrieve the value for key ‘0’, but there is no such key in the object, so the result is undefined.
 
 // This code should log out the houseNumber from the address object
 // but it isn't working...
@@ -12,4 +14,4 @@ const address = {
   postcode: "XYZ 123",
 };
 
-console.log(`My house number is ${address[0]}`);
+console.log(`My house number is ${address.houseNumber}`);
