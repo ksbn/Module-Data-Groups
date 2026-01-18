b88eca3a943f34f49bd2eecdcf6c7071ad58d16b
Anastasiia Hlushkova <glushkova.a.y@gmail.com>
2026-01-18 18:04:13 +0100

    Implemented functions: contains, lookup, querystring, tally

--- Sprint-2/implement/querystring.js
+++ Sprint-2/implement/querystring.js
@@ -1,13 +1,22 @@
 function parseQueryString(queryString) {
   const queryParams = {};
-  if (queryString.length === 0) {
+
+  if (!queryString) {
     return queryParams;
   }
+
   const keyValuePairs = queryString.split("&");
 
   for (const pair of keyValuePairs) {
-    const [key, value] = pair.split("=");
-    queryParams[key] = value;
+    const indexOfEquals = pair.indexOf("=");
+    
+    if (indexOfEquals === -1) {
+      queryParams[pair] = "";
+    } else {
+      const key = pair.slice(0, indexOfEquals);
+      const value = pair.slice(indexOfEquals + 1);
+      queryParams[key] = value;
+    }
   }
 
   return queryParams;
--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -1,3 +1,20 @@
-function tally() {}
+function tally(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("Input must be an array");
+  }
 
-module.exports = tally;
+  const counts = {};
+
+  for (let i = 0; i < arr.length; i++) {
+    const item = arr[i];
+    if (counts[item]) {
+        counts[item] += 1;
+    } else {
+        counts[item] = 1;
+    }
+  }
+
+  return counts;
+}
+
+module.exports = tally; 
